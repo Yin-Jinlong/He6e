265217fb5f61eae9ff47bd20662bf1d3be929d23
yjl <yjl_1567@qq.com>
2024-03-16 17:02:38 +0800

    提取ui，添加ui项目

--- web/pages/vite.config.ts
+++ web/pages/vite.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import obfuscator from 'rollup-plugin-obfuscator'
 import path from "node:path";
 
 // https://vitejs.dev/config/
@@ -19,14 +18,6 @@ export default defineConfig({
         reportCompressedSize: false
     },
     plugins: [
-        vue(),
-        obfuscator({
-            options: {
-                controlFlowFlattening: false,
-                deadCodeInjectionThreshold: 0,
-                stringArray: false,
-                renameProperties: true
-            }
-        })
+        vue()
     ],
 });
