c14680fe631921dc862ca71495743edb53211a30
yjl <yjl_1567@qq.com>
2024-03-17 17:57:43 +0800

    play首页使用button

--- web/ui/play/src/pages/home/src/HomePage.vue
+++ web/ui/play/src/pages/home/src/HomePage.vue
@@ -1,16 +1,30 @@
 <template>
     <ul>
         <li v-for="page in pages" :key="page.name">
-            <router-link :to="page.path">{{ page.name }}</router-link>
+            <router-link :to="page.path">
+                <yjl-button>
+                    {{ page.name }}
+                </yjl-button>
+            </router-link>
         </li>
     </ul>
 </template>
 
 <style scoped lang="scss">
+li {
+  display : inline-block;
+  margin  : 0.5em 5px;
 
+  &::marker {
+    content : '';
+    display : none;
+  }
+}
 </style>
 
 <script setup lang="ts">
+
+import {YjlButton} from "@ui";
 
 interface Page {
     name: string
