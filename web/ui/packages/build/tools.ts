827c627325f29e731e8013928f40928a5329d760
yjl <yjl_1567@qq.com>
2024-03-18 14:41:34 +0800

    添加转换浮点时间支持

--- web/ui/packages/build/tools.ts
+++ web/ui/packages/build/tools.ts
@@ -26,7 +26,7 @@ export function outln(...text: (string | Uint8Array)[]) {
  * @param time 时间
  */
 export function convertTime(time: number): string {
-    let ms = time % 1000
+    let ms = Math.round(time % 1000)
     time = Math.floor(time / 1000)
     let s = time % 60
     time = Math.floor(time / 60)
