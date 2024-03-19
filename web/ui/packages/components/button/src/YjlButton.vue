5c0f3987b5d3f4560f6bf2d33c42cc069f44de6f
yjl <yjl_1567@qq.com>
2024-03-19 22:35:17 +0800

    提取button函数工具

--- web/ui/packages/components/button/src/YjlButton.vue
+++ web/ui/packages/components/button/src/YjlButton.vue
@@ -15,10 +15,10 @@
 </style>
 
 <script lang="ts" setup>
-import chroma, {hsl, Color} from 'chroma-js'
 import {YjlButtonProps} from "./yjl-button"
 import {onMounted, ref, watch} from "vue"
-import {DefinedNamedColor} from "components/types";
+import {DefinedNamedColor} from "components/types"
+import {genColor} from "./color-tool"
 
 const btn = ref<HTMLButtonElement>()
 
@@ -26,37 +26,15 @@ const props = withDefaults(defineProps<YjlButtonProps>(), {
     size: 'default',
 })
 
-const namedColors = ['primary', 'success', 'warning', 'danger', 'info', 'emphasize']
-
-const btnColors: (string | Color)[] = []
-
-function genColor(color: string | DefinedNamedColor) {
-    btnColors.length = 0
-    if (namedColors.includes(color)) {
-        btnColors.push(`var(--color-${color})`)
-        for (let i = 1; i <= 6; i++)
-            btnColors.push(`var(--color-${color}-${i})`)
-        for (let i = 1; i <= 5; i++)
-            btnColors.push(`var(--color-${color}--${i})`)
-    } else {
-        let c = chroma(color).hsl()
-        btnColors.push(hsl(...c))
-        for (let i = 1; i <= 6; i++)
-            btnColors.push(hsl(c[0], c[1], c[2] + 0.05 * i))
-        for (let i = 1; i <= 5; i++)
-            btnColors.push(hsl(c[0], c[1], c[2] - 0.05 * i))
-    }
-    return color
-}
 
 function changeColor(color: string | DefinedNamedColor) {
-    genColor(color)
+    let btnColors = genColor(color, 6, 5)
     const style = btn.value!.style
-    style.setProperty('--btn-color', btnColors[0].toString())
+    style.setProperty('--btn-color', btnColors[0])
     for (let i = 1; i <= 6; i++)
-        style.setProperty(`--btn-color-${i}`, btnColors[i].toString())
+        style.setProperty(`--btn-color-${i}`, btnColors[i])
     for (let i = 1; i <= 5; i++)
-        style.setProperty(`--btn-color--${i}`, btnColors[6 + i].toString())
+        style.setProperty(`--btn-color--${i}`, btnColors[6 + i])
 }
 
 onMounted(() => {
